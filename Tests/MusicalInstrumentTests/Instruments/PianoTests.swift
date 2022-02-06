15fdc53436969e08e1c5ba5c3d6f546af901d565
live77 <live77>
2022-02-06 21:47:06 +0800

    test Piano syncopated pedal effect. test Piano pedal is worked.

--- Tests/MusicalInstrumentTests/Instruments/PianoTests.swift
+++ Tests/MusicalInstrumentTests/Instruments/PianoTests.swift
@@ -69,5 +69,87 @@ class PianoTests: XCTestCase {
         Thread.sleep(forTimeInterval: 1.0)
         piano.stop(at: "C4")
     }
+
+    
+    /// test piano with pedal
+    func testPianoWithPedal() {
+        let piano = Piano.default
+        
+        // first note, without padel
+        print("no padel")
+        piano.play(at: .C4, with: NotePlayGuidance(playIntenisty: 60))
+        Thread.sleep(forTimeInterval: 0.10)
+        piano.stop(at: .C4)
+        Thread.sleep(forTimeInterval: 2.0)
+        
+        // padal on , then press the key
+        print("use padel")
+        piano.pedalOn()
+        piano.play(at: .C4, with: NotePlayGuidance(playIntenisty: 60))
+        Thread.sleep(forTimeInterval: 0.10)
+        piano.stop(at: .C4)
+        // duration the following 2 seconds, sound still exists
+        Thread.sleep(forTimeInterval: 2.0)
+        
+        // release the padel, sounds off
+        print("padel off")
+        piano.pedalOff()
+        Thread.sleep(forTimeInterval: 2.0)
+        
+        piano.stopAll()
+    }
+
+    /// test pedal switch sound effect
+    func testPedalWithChord() {
+        let piano = Piano.default
+
+        print("padel on")
+        piano.pedalOn()
+        piano.play(at: "C4", with: .intensity(60))
+        piano.play(at: "E4", with: .intensity(60))
+        piano.play(at: "G4", with: .intensity(60))
+        Thread.sleep(forTimeInterval: 0.1)
+        piano.stop(at: "C4")
+        piano.stop(at: "E4")
+        piano.stop(at: "G4")
+        Thread.sleep(forTimeInterval: 1.0)
+
+        // play
+        piano.play(at: "D4", with: .intensity(60))
+        Thread.sleep(forTimeInterval: 0.1)
+        piano.stop(at: "D4")
+        Thread.sleep(forTimeInterval: 2.0)
+
+        print("pedal off")
+        piano.pedalOff()
+        Thread.sleep(forTimeInterval: 2.0)
+    }
+    
+    // syncopated pedal
+    func testSyncopatedPedalEffect() {
+        let piano = Piano.default
+        
+        print("padel on")
+        piano.pedalOn()
+        piano.play(at: "C4", with: .intensity(60))
+        piano.play(at: "E4", with: .intensity(60))
+        piano.play(at: "G4", with: .intensity(60))
+        Thread.sleep(forTimeInterval: 0.1)
+        piano.stopAll()
+        Thread.sleep(forTimeInterval: 1.0)
+
+        // off and quickly on the padel
+        piano.play(at: "D4", with: .intensity(60))
+        piano.pedalOff()
+        piano.pedalOn()
+        Thread.sleep(forTimeInterval: 0.1)
+        piano.stop(at: "D4")
+
+        // C chord sound is not exist , but D exists
+        Thread.sleep(forTimeInterval: 2.0)
+
+        piano.stopAll()
+    }
+
 }
 
